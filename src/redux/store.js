22713666a25f5c33388646413423771d64888891
AmirNavazi <navazi.amir@gmail.com>
2024-01-04 14:29:23 +0330

    middleWare-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,8 @@
 
 import { legacy_createStore as createStore} from "@reduxjs/toolkit"
 import rootReducer from "./rootReducer";
+import { applyMiddleware } from "redux";
+import logger from "redux-logger";
 
-const store= createStore(rootReducer)
+const store= createStore(rootReducer,applyMiddleware(logger))
 export default store;
